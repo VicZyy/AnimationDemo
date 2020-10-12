d974c5923a84a5b1089945144c9d7d50e08a7920
Vic <Vic@DESKTOP-7M8Q4RJ>
2020-10-12 17:41:57 +0800

    修改人物动画状态机，添加人物的前进、后退、跳的动画控制逻辑

--- Assets/Scripts/VicAnimator.cs
+++ Assets/Scripts/VicAnimator.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class VicAnimator : MonoBehaviour
 {
+    public Animator _animator;
+    private AnimatorStateInfo _stateInfo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //跑
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _animator.SetBool("run", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            _animator.SetBool("run", false);
+        }
+        //后退
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _animator.SetBool("back", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            _animator.SetBool("back", false);
+        }
+        _stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        //跳
+        if (_stateInfo.shortNameHash == Animator.StringToHash("RunForward") && Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("jump");
+        }
     }
 }
